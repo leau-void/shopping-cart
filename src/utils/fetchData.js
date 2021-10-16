d5f9343f45887b71e33fafdffd99e709d7d53285
leau-void <lpierra@gmail.com>
2021-10-16 11:12:30 -0300

    fetchData removesScript onload so no memory leaks

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,6 +1,7 @@
 const fetchData = (src) => {
   const s = document.createElement("script");
   s.setAttribute("src", src);
+  s.onload = () => document.body.removeChild(s);
   document.body.appendChild(s);
 };
 
